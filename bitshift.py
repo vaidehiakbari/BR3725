080dbf9373914a7f617826c2fad18fdaf74e4e33
vaidehiakbari <akbari4846@mydusd.org>
2025-03-07 13:31:20 -0800

    Update bitshift.py

--- bitshift.py
+++ bitshift.py
@@ -1,5 +1,9 @@
-def multiply(n):
-    #Add the code and an explanation on how to 
-    #multiply a number by 2 using bitshift
-    return n
+def multiply(a, b):
+    result = 0 #Add the code and an explanation on how to 
+    while b > 0:
+        if b is odd:
+            result = result + a  #multiply a number by 2 using bitshift
+            b = b >> 1  
+        a = a << 1
+    return result
 
